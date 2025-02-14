3a9c6b4cc393e1ff48ad2bce0261bbe4338f7720
Zhanna Suianina <cezyby3@gmail.com>
2025-02-14 15:56:30 +0200

    registry correction

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,12 +4,14 @@ import iziToast from "izitoast";
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "48789151-f51ae4d97727bedfe2d09c642";
 
+
 export async function fetchImages(value) {
-    console.log("Запускаем fetchImages() с запросом:", value);
+    const query = value.trim();
+    console.log("Отправляем запрос:", query)
 
     const params = {
         key: API_KEY,
-        q: value,
+        q: query,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true,
@@ -18,9 +20,9 @@ export async function fetchImages(value) {
     try {
         const response = await axios.get(BASE_URL, { params });
         console.log("Ответ от API:", response.data);
-        return response.data;
+        return response.data; 
     } catch (error) {
-        console.error("Ошибка при запросе к API:", error);
+        console.error("Ошибка API:", error);
         iziToast.error({
             position: "topRight",
             message: "Error fetching images. Please try again!",
